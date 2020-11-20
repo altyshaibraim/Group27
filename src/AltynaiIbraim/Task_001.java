5d052533bad632d7148e863403f133f2bb6234d2
altyshaibraim <altyshaibraim@gmail.com>
2020-11-19 22:29:10 -0600

    created task_001 file edited it

--- src/AltynaiIbraim/Task_001.java
+++ src/AltynaiIbraim/Task_001.java
@@ -3,5 +3,6 @@ package AltynaiIbraim;
 public class Task_001 {
     public static void main(String[] args) {
         System.out.println("Good morning!");
+        System.out.println("jdnewfefhefefjefbe");
     }
 }
